9486e306a61885cc137142a31f9fa1f0dd9a9dd8
Alex Standiford <a@alexstandiford.com>
2022-12-18 10:27:17 -0600

    Adds Request builder class, and stubs.

---
+++ lib/Builders/Request_Builder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Adiungo\Integrations\WordPress\Builders;
+
+use Adiungo\Core\Abstracts\Int_Id_Based_Request_Builder;
+
+class Request_Builder extends Int_Id_Based_Request_Builder
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function get_id(): ?int
+    {
+        return null;
+    }
+
+    /**
+     * Sets the ID.
+     * @param ?int $id The ID to set
+     *
+     * return $this
+     */
+    public function set_id(?int $id): static
+    {
+        return $this;
+    }
+}
