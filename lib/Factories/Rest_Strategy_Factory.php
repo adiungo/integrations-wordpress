91e73fc6c99a94e59393ee613701052560ade856
Alex Standiford <a@alexstandiford.com>
2022-12-18 15:48:51 -0600

    Implements rest strategy factory, and stubs. (#21)

---
+++ lib/Factories/Rest_Strategy_Factory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Adiungo\Integrations\WordPress\Factories;
+
+
+use Adiungo\Core\Factories\Data_Sources\Rest;
+use Adiungo\Core\Factories\Index_Strategy;
+use Adiungo\Core\Interfaces\Has_Http_Strategy;
+use Adiungo\Core\Traits\With_Http_Strategy;
+use DateTime;
+use Underpin\Factories\Url;
+use Underpin\Registries\Param_Collection;
+
+class Rest_Strategy_Factory implements Has_Http_Strategy
+{
+    use With_Http_Strategy;
+
+    /**
+     * Assembles the items specific to this integration that are always the same.
+     *
+     * @return Rest
+     */
+    protected function get_instance_template(): Rest
+    {
+        return new Rest();
+    }
+
+    public function build(Url $base, DateTime $last_requested, ?Param_Collection $batch_query_params = null): Index_Strategy
+    {
+        return new Index_Strategy();
+    }
+
+    /**
+     * Builds the instance.
+     * Note that this does NOT implement set the HTTP strategy. That is up to the platform to set.
+     */
+    protected function build_data_source(Url $base, DateTime $last_requested): Rest
+    {
+        return new Rest();
+    }
+}
---
+++ tests/Unit/Factories/Rest_Strategy_Factory_Test.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Adiungo\Integrations\WordPress\Tests\Unit\Factories;
+
+
+use Adiungo\Tests\Test_Case;
+
+class Rest_Strategy_Factory_Test extends Test_Case {
+    /**
+     * @covers \Adiungo\Integrations\WordPress\Factories\Rest_Strategy_Factory::get_instance_template
+     * @return void
+     */
+    public function test_can_get_instance_template(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \Adiungo\Integrations\WordPress\Factories\Rest_Strategy_Factory::build
+     * @return void
+     */
+    public function test_can_build(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \Adiungo\Integrations\WordPress\Factories\Rest_Strategy_Factory::build_data_source
+     * @return void
+     */
+    public function test_can_build_data_source(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
