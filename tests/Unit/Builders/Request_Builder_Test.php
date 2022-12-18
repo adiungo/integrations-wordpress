5a465cef745000a3cecf6e494036e31a851ec033
Alex Standiford <a@alexstandiford.com>
2022-12-18 10:28:18 -0600

    Adds Request builder class, and stubs. (#17)

---
+++ tests/Unit/Builders/Request_Builder_Test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Adiungo\Integrations\WordPress\Tests\Unit\Builders;
+
+
+use Adiungo\Tests\Test_Case;
+
+class Request_Builder_Test extends Test_Case
+{
+
+    /**
+     * @covers \Adiungo\Integrations\WordPress\Builders\Request_Builder::get_id
+     * @return void
+     */
+    public function test_can_get_id()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \Adiungo\Integrations\WordPress\Builders\Request_Builder::set_id
+     * @return void
+     */
+    public function test_can_set_id(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
