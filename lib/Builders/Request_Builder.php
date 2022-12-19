550ceda23fad026eb4c4e2f752329796ebc83c91
Alex Standiford <a@alexstandiford.com>
2022-12-18 20:08:03 -0600

    Implements Request builder and tests

--- lib/Builders/Request_Builder.php
+++ lib/Builders/Request_Builder.php
@@ -3,6 +3,11 @@
 namespace Adiungo\Integrations\WordPress\Builders;
 
 use Adiungo\Core\Abstracts\Int_Id_Based_Request_Builder;
+use Underpin\Enums\Types;
+use Underpin\Exceptions\Operation_Failed;
+use Underpin\Exceptions\Unknown_Registry_Item;
+use Underpin\Exceptions\Validation_Failed;
+use Underpin\Factories\Registry_Items\Param;
 
 class Request_Builder extends Int_Id_Based_Request_Builder
 {
@@ -11,7 +16,11 @@ class Request_Builder extends Int_Id_Based_Request_Builder
      */
     public function get_id(): ?int
     {
-        return null;
+        try {
+            return $this->get_request()->get_url()->get_params()->get('id')->get_value();
+        } catch (Unknown_Registry_Item $exception) {
+            return null;
+        }
     }
 
     /**
@@ -19,9 +28,20 @@ class Request_Builder extends Int_Id_Based_Request_Builder
      * @param ?int $id The ID to set
      *
      * return $this
+     * @return static
      */
     public function set_id(?int $id): static
     {
+        if (!$id) {
+            try {
+                $this->get_request()->get_url()->remove_param('id');
+            } catch (Operation_Failed $e) {
+                return $this;
+            }
+        } else {
+            $this->get_request()->set_param((new Param('id', Types::Integer))->set_value($id));
+        }
+
         return $this;
     }
 }
