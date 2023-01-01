e8d26636f5852f22481c6a1c195239d70ade7456
Alex Standiford <alex@standiford.us>
2022-12-31 19:53:51 -0600

    Implements build data source method.

--- lib/Factories/Post_Rest_Strategy_Factory.php
+++ lib/Factories/Post_Rest_Strategy_Factory.php
@@ -4,49 +4,122 @@ namespace Adiungo\Integrations\WordPress\Factories;
 
 use Adiungo\Core\Factories\Data_Sources\Rest;
 use Adiungo\Core\Factories\Index_Strategy;
+use Adiungo\Core\Factories\Updated_Date_Strategy;
 use Adiungo\Core\Interfaces\Has_Http_Strategy;
 use Adiungo\Core\Interfaces\Has_Index_Strategy;
 use Adiungo\Core\Traits\With_Http_Strategy;
 use Adiungo\Core\Traits\With_Index_Strategy;
 use DateTime;
+use Underpin\Enums\Types;
+use Underpin\Exceptions\Operation_Failed;
+use Underpin\Exceptions\Unknown_Registry_Item;
+use Underpin\Exceptions\Validation_Failed;
+use Underpin\Factories\Registry_Items\Param;
+use Underpin\Factories\Request;
 use Underpin\Factories\Url;
 use Underpin\Registries\Param_Collection;
 
 class Post_Rest_Strategy_Factory implements Has_Http_Strategy, Has_Index_Strategy
 {
-    use With_Http_Strategy;
-    use With_Index_Strategy;
+	use With_Http_Strategy;
+	use With_Index_Strategy;
 
-    /**
-     * Assembles the items specific to this integration that are always the same.
-     *
-     * @return Rest
-     */
-    protected function get_instance_template(): Rest
-    {
-        return new Rest();
-    }
+	/**
+	 * Assembles the items specific to this integration that are always the same.
+	 *
+	 * @return Rest
+	 */
+	protected function get_instance_template() : Rest
+	{
+		return new Rest();
+	}
 
-    /**
-     * Builds the index strategy
-     *
-     * @param Url $base The base URL to use to fetch content. Usually this is the REST endpoint used to fetch bulk content.
-     * @param DateTime $last_requested The date/time this content was last requested. Use GMT timezone.
-     * @param Param_Collection|null $batch_query_params Any custom query params that need to be included on batch requests.
-     * @return Index_Strategy
-     */
-    public function build(Url $base, DateTime $last_requested, ?Param_Collection $batch_query_params = null): Index_Strategy
-    {
-        return (new Index_Strategy())
-            ->set_data_source($this->build_data_source($base, $last_requested, $batch_query_params));
-    }
+	/**
+	 * Builds the index strategy
+	 *
+	 * @param Url                   $base               The base URL to use to fetch content. Usually this is the REST
+	 *                                                  endpoint used to fetch bulk content.
+	 * @param DateTime              $last_requested     The date/time this content was last requested. Use GMT timezone.
+	 * @param Param_Collection|null $batch_query_params Any custom query params that need to be included on batch
+	 *                                                  requests.
+	 *
+	 * @return Index_Strategy
+	 * @throws Operation_Failed
+	 * @throws Validation_Failed
+	 */
+	public function build(Url $base, DateTime $last_requested, ?Param_Collection $batch_query_params = null) : Index_Strategy
+	{
+		return (new Index_Strategy())
+			->set_data_source($this->build_data_source($base, $last_requested, $batch_query_params));
+	}
 
-    /**
-     * Builds the instance.
-     * Note that this does NOT implement set the HTTP strategy. That is up to the platform to set.
-     */
-    protected function build_data_source(Url $base, DateTime $last_requested, ?Param_Collection $batch_query_params): Rest
-    {
-        return new Rest();
-    }
+	/**
+	 * Builds the has more strategy using the provided date.
+	 *
+	 * @param DateTime $last_requested The last requested date. Use GMT.
+	 *
+	 * @return Updated_Date_Strategy
+	 */
+	protected function build_has_more_strategy(DateTime $last_requested) : Updated_Date_Strategy
+	{
+		return (new Updated_Date_Strategy())->set_updated_date($last_requested);
+	}
+
+	/**
+	 * Builds the batch request.
+	 *
+	 * @param Url                   $base
+	 * @param Param_Collection|null $batch_query_params
+	 *
+	 * @return Request
+	 * @throws Operation_Failed
+	 * @throws Validation_Failed
+	 */
+	protected function build_batch_request(Url $base, ?Param_Collection $batch_query_params = null) : Request
+	{
+		$base->get_params()->merge($batch_query_params);
+
+		// Set the order and order by params.
+		try {
+			$base->get_params()->get('orderby');
+		} catch (Unknown_Registry_Item $e) {
+			$base->add_param((new Param('orderby', Types::String))->set_value('modified'));
+		}
+
+		try {
+			$base->get_params()->get('order');
+		} catch (Unknown_Registry_Item $e) {
+			$base->add_param((new Param('order', Types::String))->set_value('desc'));
+		}
+
+		return (new Request())->set_url($base);
+	}
+
+	/**
+	 * Builds the single request.
+	 *
+	 * @param Url $base
+	 *
+	 * @return Request
+	 */
+	protected function build_single_request(Url $base) : Request
+	{
+		return (new Request())->set_url($base);
+	}
+
+	/**
+	 * Builds the instance.
+	 * Note that this does NOT implement set the HTTP strategy. That is up to the platform to set.
+	 *
+	 * @throws Operation_Failed|Validation_Failed
+	 */
+	protected function build_data_source(Url $base, DateTime $last_requested, ?Param_Collection $batch_query_params = null) : Rest
+	{
+		$result = $this->get_instance_template();
+		$result->get_single_request_builder()->set_request($this->build_single_request($base));
+		$result->get_batch_request_builder()->set_request($this->build_batch_request($base, $batch_query_params));
+		$result->set_has_more_strategy($this->build_has_more_strategy($last_requested));
+
+		return $result;
+	}
 }
--- tests/Unit/Factories/Post_Rest_Strategy_Factory_Test.php
+++ tests/Unit/Factories/Post_Rest_Strategy_Factory_Test.php
@@ -3,14 +3,20 @@
 namespace Adiungo\Integrations\WordPress\Tests\Unit\Factories;
 
 use Adiungo\Core\Factories\Data_Sources\Rest;
+use Adiungo\Core\Factories\Updated_Date_Strategy;
 use Adiungo\Integrations\WordPress\Factories\Post_Rest_Strategy_Factory;
 use Adiungo\Tests\Test_Case;
+use Adiungo\Tests\Traits\With_Inaccessible_Methods;
 use DateTime;
 use Mockery;
+use Underpin\Factories\Request;
 use Underpin\Factories\Url;
+use Underpin\Registries\Param_Collection;
 
 class Post_Rest_Strategy_Factory_Test extends Test_Case
 {
+    use With_Inaccessible_Methods;
+
     /**
      * @covers \Adiungo\Integrations\WordPress\Factories\Post_Rest_Strategy_Factory::get_instance_template
      * @return void
@@ -45,6 +51,31 @@ class Post_Rest_Strategy_Factory_Test extends Test_Case
      */
     public function test_can_build_data_source(): void
     {
-        $this->markTestIncomplete();
+        $instance = Mockery::mock(Post_Rest_Strategy_Factory::class)
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+
+        $base = Url::from('baz.com');
+        $batch_query_params = new Param_Collection();
+        $last_requested = new DateTime('10 minutes ago');
+
+        $expected = Mockery::mock(Rest::class);
+
+        $single_request = (new Request())->set_url(Url::from('foo.com'));
+        $batch_request = (new Request())->set_url(Url::from('bar.com'));
+        $has_more_strategy = new Updated_Date_Strategy();
+
+        $expected->expects('get_single_request_builder->set_request')->with($single_request);
+        $expected->expects('get_batch_request_builder->set_request')->with($batch_request);
+        $expected->expects('set_has_more_strategy')->with($has_more_strategy);
+
+        $instance->allows('build_single_request')->andReturn($single_request);
+        $instance->allows('build_batch_request')->with($base, $batch_query_params)->andReturn($batch_request);
+        $instance->allows('build_has_more_strategy')->with($last_requested)->andReturn($has_more_strategy);
+        $instance->allows('get_instance_template')->andReturn($expected);
+
+        $result = $this->call_inaccessible_method($instance, 'build_data_source', $base, $last_requested, $batch_query_params);
+
+        $this->assertSame($expected, $result);
     }
 }
